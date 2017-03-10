64b6ae95d239fd9fac2abb84c5fec98b0ef25e0d
jeaninne33 <jeaninne33@gmail.com>
2017-03-10 15:54:10 -0430

    10/03 jea

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('contenido')
-<div ng-controller="EditCompanyCtrl" class="col-sm-12 no-float no-padding" ng-init="company={{$companys}}; airplanes={{$companys->aviones}}; " >
-      <h2>Editar Compañia <strong> @{{ company.nombre }}</strong></h2>
+<div ng-controller="EditCompanyCtrl" class="col-sm-12 no-float no-padding" ng-init="invoice={{json_encode($invoice)}}; data_invoices={{json_encode($items)}}">
+      <h2>Editar Factura Número<strong> @{{ $invoice.id }}</strong></h2>
       <div class="pull-right">
-               <a class="btn btn-primary" href="{{ route('companys.index') }}"> Atrás</a>
+               <a class="btn btn-primary" href="{{ route('invoices.index') }}"> Atrás</a>
       </div>
 <p style="color:rgb(235, 160, 162)">Los campos con (*) son Obligatorios</p>
 
 <br/>
 <!-- if there are creation errors, they will show here ng-init=""-->
 
-
+{{var_dump($estimate)}}
     @include('errors.message')
-   [[Form::model($companys, array('route' => array('companys.update', $companys->id), 'method' => 'PUT','ng-submit'=>'edit($event)', 'novalidate'))]]
-    @include('companys.partials.fields')
+   [[Form::model($invoice, array('route' => array('companys.update', $invoice->id), 'method' => 'PUT','ng-submit'=>'edit($event)', 'novalidate'))]]
+      @include('invoices.partials.fields')
   [[ Form::submit('Editar Compañia', array('class' => 'btn btn-primary')) ]]
   [[ Form::close() ]]
 </div>
@@ -23,23 +23,8 @@
 @section('scripts')
 
 <script>
-  $('#m2').removeClass('');
-  $('#m2').addClass('active');
-   var tipo=$('#tipo').val();
-  // alert(tipo);
-  if(tipo=="prove"){
-     $(".cliente").css("display", "none");
-     $(".proveedor").css("display", "block");
-     $(".avion").css("display", "none");
-  }else if (tipo=="client") {
-      $(".cliente").css("display", "block");
-      $(".proveedor").css("display", "none");
-      $(".avion").css("display", "block");
- }else{
-      $(".cliente").css("display", "block");
-      $(".proveedor").css("display", "block");
-      $(".avion").css("display", "block");
-  }//fin si
+$('#m5').removeClass('');
+$('#m5').addClass('active');
 </script>
 
 @endsection
